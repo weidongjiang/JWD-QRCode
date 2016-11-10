22111f3c1099f37473ca4e4765a43ab8d9994364
蒋伟东 <jiangweidong@yixia.com>
2016-11-10 10:15:26 +0800

    创建项目

---
+++ JWDQRCode/JWDQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWDQRCode
+//
+//  Created by 蒋伟东 on 2016/11/10.
+//  Copyright © 2016年 YIXIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWDQRCode/JWDQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWDQRCode
+//
+//  Created by 蒋伟东 on 2016/11/10.
+//  Copyright © 2016年 YIXIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
